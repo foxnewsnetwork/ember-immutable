ee1402461be9c4c874c7ba23342a34158af53eaf
Thomas Chen <tom.chen@am.sony.com>
2017-03-28 12:21:16 -0700

    upgraded to using ember-cli-node-assets and removed broccoli tree stuff

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
 /* jshint node: true */
 'use strict';
 
-const Funnel = require('broccoli-funnel')
-const mergeTrees = require('broccoli-merge-trees')
-const path = require('path')
-
 // Stolen shamelessly from https://github.com/jasonmit/ember-cli-moment-shim/blob/master/index.js
 module.exports = {
-  name: 'immutable',
+  name: 'ember-immutable',
 
-  included(app) {
-    this._super.included.apply(this, arguments);
-
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    while (typeof app.import !== 'function' && app.app) {
-      app = app.app;
+  options: {
+    nodeAssets: {
+      'immutable': {
+        vendor: ['dist/immutable.js']
+      }
     }
-
-    this.app = app;
-
-    const vendor = this.treePaths.vendor;
-
-    app.import(vendor + '/immutable/immutable.js', { prepend: true });
-    app.import(vendor + '/shims/immutable.js');
-    return app;
   },
 
-  treeForVendor(vendorTree) {
-    const immutableRoot = require.resolve('immutable');
-    const immutablePath = path.dirname(immutableRoot);
+  included() {
+    this._super.included.apply(this, arguments);
 
-    let trees = [];
-
-    if (vendorTree) {
-      trees.push(vendorTree);
-    }
-
-    trees.push(new Funnel(immutablePath, {
-      destDir: 'immutable',
-      include: [new RegExp(/\.js$/)]
-    }));
-
-    return mergeTrees(trees);
+    this.import('vendor/immutable/dist/immutable.js', {
+      using: [{ transformation: 'amd', as: 'immutable' }]
+    });
   }
 };
